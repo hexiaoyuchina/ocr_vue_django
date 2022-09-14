547d4cf4ed6aac6c33db7aaa970d9a57dba90245
hexiaoyu <xiaoyu.he@capitalonline.net>
2022-09-14 11:26:24 +0800

    全局

--- ocr_django/api/views/recognize_img.py
+++ ocr_django/api/views/recognize_img.py
@@ -2,7 +2,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
+from rest_framework.response import Response
 from service.RegcongizeService import RegcongizeService
 
 class RegeViewSets(viewsets.GenericViewSet):
@@ -10,4 +10,6 @@ class RegeViewSets(viewsets.GenericViewSet):
     def reg_image(self, request, *args, **kwargs):
         action = RegcongizeService()
         res = action.reg_imge(request)
+        # image_data = open(imagepath, "rb").read()
+        # return HttpResponse(image_data, content_type="image/png")
         return Response(res)
